838d66cffe7ce5f0b945a11fb87d157f60784503
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-06-30 12:30:12 +0100

    mainPageController  and main.java
    started main page controller.
    main.java now opens the program on the main page.

--- Image-Editor/src/Main.java
+++ Image-Editor/src/Main.java
@@ -1,13 +1,36 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * 
  * @author Matthew Payne
  *
  */
-public class Main {
+public class Main extends Application {
 
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		// sets the root to the log in page.
+		Parent root = FXMLLoader.load(getClass().getResource("Main page.fxml"));
+		// creates the log in page scene.
+		Scene scene = new Scene(root);
+		// sets and displays the scene to the user.
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	
+	}
+
+	/**
+	 *  Launches the program.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 }
---
+++ Image-Editor/src/mainPageController.java
@@ -0,0 +1,40 @@
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
+
+/**
+ * 
+ * @author Matthew Payne
+ *
+ */
+public class mainPageController {
+	@FXML MenuItem closeFile;
+	@FXML MenuItem insertImageEdit;
+	@FXML Canvas canvasMain;
+	@FXML Button straightLineBtn;
+	@FXML MenuItem saveAsFile;
+	@FXML MenuItem newFile;
+	@FXML Button freeLineBtn;
+	@FXML VBox vBoxMain;
+	@FXML MenuBar menuBarMain;
+	@FXML Menu editMain;
+	@FXML Slider sizeSliderMain;
+	@FXML ColorPicker colourPickerMain;
+	
+	public void initialize() {
+		
+	}
+	
+	private void handleFreeLineBtn() {
+		
+	}
+	
+	
+	
+}
