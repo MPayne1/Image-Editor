17f5023a8eaf59479cbfafd041915f30a223e297
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-07-02 22:53:07 +0100

    exit without saving

--- Image-Editor/src/Main.java
+++ Image-Editor/src/Main.java
@@ -1,10 +1,18 @@
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 /**
  * 
@@ -16,9 +24,21 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws IOException {
 		Parent mainBorderPane = FXMLLoader.load(getClass().getClassLoader().getResource("mainPage.fxml"));
 		Scene scene = new Scene(mainBorderPane);
+		primaryStage.initStyle(StageStyle.DECORATED);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Image Editor");
 		primaryStage.show();
+		
+		// handle exit without saving
+		Platform.setImplicitExit(false);
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				handleWindowClose(event);
+		    }
+		
+		});
+		
 	}
 
 	/**
@@ -29,4 +49,20 @@ public class Main extends Application {
 		launch(args);
 	}
 
+	private void handleWindowClose(WindowEvent event) {
+		 Alert alert = new Alert(AlertType.CONFIRMATION);
+	    	alert.setTitle("Exit without saving?");
+	    	alert.setHeaderText("Do you want to exit without saving");
+	    	alert.setContentText("Are you ok with this?");
+	    	
+	    	ButtonType cancel = new ButtonType("Cancel");
+	    	ButtonType exit = new ButtonType("Exit without Saving");
+	    	alert.getButtonTypes().setAll(exit,cancel);
+	    	Optional<ButtonType> result = alert.showAndWait();
+	    	if (result.get() == cancel){
+	    	    event.consume(); 
+	    	} else if(result.get() == exit) {
+	    	   Platform.exit();
+	    	}
+	}
 }
--- Image-Editor/src/mainPageController.java
+++ Image-Editor/src/mainPageController.java
@@ -1,27 +1,35 @@
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.imageio.ImageIO;
 
+import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 
 /**
  * 
@@ -55,6 +63,8 @@ public class mainPageController {
 	ColorPicker colourPicker;
 	@FXML
 	Button eraserBtn;
+	@FXML private BorderPane bPane;
+	
 	
 	private double fromX;
 	private double fromY;
@@ -126,6 +136,8 @@ public class mainPageController {
 			this.canvasMain.setOnMouseReleased(release -> {
 			});
 		});
+		
+		
 	}
 	
 	/**
@@ -184,5 +196,6 @@ public class mainPageController {
 			ImageIO.write(renderedImage, "png", file);
 		}
 	}
+	
 	
 }
